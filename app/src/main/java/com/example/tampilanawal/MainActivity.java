31036f0449ab2421be1a43c060f64ca3916ffb7d
khoirulwhyd <gamingkenzo7@gmail.com>
2021-06-08 22:25:48 +0700

    projek android

---
+++ app/src/main/java/com/example/tampilanawal/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tampilanawal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan_awal);
+    }
+}
---
+++ app/src/main/java/com/example/tampilanawal/klik_daftar.java
@@ -0,0 +1,14 @@
+package com.example.tampilanawal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class klik_daftar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_klik_daftar);
+    }
+}
